de9fc503d79c6bb11fe1dfd9aba017391b4cafd6
sophiamatthews <140354061+sophiamatthews@users.noreply.github.com>
2023-08-06 23:02:14 +1200

    Bear
    outline of bear with pink background

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,12 +1,12 @@
-//your parameter variables go here!
-let rect_width  = 20;
+//your parameter variables go here
+let rect_width  = 60;
 let rect_height = 20;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GLIDE_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -15,9 +15,23 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(255,182,193); //light pink colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  // Original
+  // rect(40 ,40, rect_width, rect_height);
+  
+  // rect(1 ,1, rect_width, rect_height);
+
+  
+  
+  fill(193, 154, 107);
+  ellipse(25,25,40,40); 
+  ellipse(75,25,40,40);
+  ellipse(50,50,80,80);
+  fill(0,0,0);
+  ellipse(30,40,13,13);
+  ellipse(70,40,13,13);
+
 }
